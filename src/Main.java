bbfefed26d5ca158630d6bcb8146674ab7e4e665
Ruan <ruan.cleitonn2001@gmail.com>
2026-07-01 19:13:29 -0300

    Interligando as classes de dominio

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import br.com.desafio.dominio.Conteudo;
-import br.com.desafio.dominio.Curso;
-import br.com.desafio.dominio.Mentoria;
+import br.com.desafio.dominio.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -10,11 +8,16 @@ public class Main {
     public static void main(String[] args) {
 
         Curso curso1 = new Curso();
+
         curso1.setNome("Curso de Java");
         curso1.setDescicao("Curso voltado para Beck-and e QA");
         curso1.setCargaHoraria(12);
 
-        System.out.println(curso1);
+        Curso curso2 = new Curso();
+
+        curso1.setNome("Curso de JS");
+        curso1.setDescicao("Curso voltado para frot-and e QA");
+        curso1.setCargaHoraria(8);
 
         Mentoria mentoria = new Mentoria();
 
@@ -22,6 +25,26 @@ public class Main {
         mentoria.setCargaHoraria(8);
         mentoria.setData(LocalDate.now());
 
-        System.out.println(mentoria);
+        Bootcamp bootcamp = new Bootcamp();
+
+        bootcamp.setNome("Bootcamp Java developer");
+        bootcamp.setDescricao("descricao bootcamp java");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev dev1 = new Dev();
+        dev1.setNome("Maria");
+        dev1.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos por Maria: " + dev1.getConteudosInscritos());
+        dev1.progredir();
+        System.out.println("Conteudos inscritos por Maria: " + dev1.getConteudosInscritos());
+        System.out.println("Conteudos concluidos por Maria: " + dev1.getConteudosConcluidos());
+        System.out.println("Xp: " + dev1.calcXpTotal());
+
+
+
+        Dev dev2 = new Dev();
+        dev2.setNome("João");
     }
 }
